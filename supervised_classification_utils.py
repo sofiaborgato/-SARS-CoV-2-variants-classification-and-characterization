5d5056de5b30930a7950d2c6102418ce264414a5
Sofia Borgato <65445501+sofiaborgato@users.noreply.github.com>
2021-06-11 15:40:34 +0200

    Update supervised_classification_utils.py

--- supervised_classification_utils.py
+++ supervised_classification_utils.py
@@ -30,7 +30,8 @@ def classifier(data,test):
         pred.append(map[line])
     pred_df=pd.DataFrame()
     pred_df['Variant']=pred
-    fig=sns.countplot(x='Variant',data=pred_df,palette='Paired')
+    plot=sns.countplot(x='Variant',data=pred_df,palette='Paired')
+    fig = plot.get_figure()
     fig.savefig('classification_report_plot.png')
     
     
