7dd57f30354a6b743bcc564de7564b0721cbf519
MarcoBott1997 <62574233+MarcoBott1997@users.noreply.github.com>
2021-07-06 10:33:47 +0200

    Update main_def.py

--- main_def.py
+++ main_def.py
@@ -2,9 +2,13 @@
 # coding: utf-8
 
 import os
-path=os.getcwd() 
 import git
 import shutil 
+import sys
+
+path=os.getcwd() #path of the working directory
+
+#The git repository with the code and the data is cloned into the working directory, if it's not already present
 if not 'Bioinformatic_project' in os.listdir(path):
     print('Downloading the Git repo...')
     git.Git(path).clone('https://github.com/sofiaborgato/Bioinformatic_project.git')
@@ -12,22 +16,21 @@ else:
     print('Bioinformatic_project already imported')
 
 os.chdir(os.path.join(path,'Bioinformatic_project'))
-
-
-import sys
-
 sys.path.append(os.path.join(path,'Bioinformatic_project'))
+
+
 from preprocessing_utils import *
 from supervised_classification_utils  import *
 from clustering_utils import nullscan
 from clustering_utils import clustering
 import tarfile
 
-# open file
+#Unzipping the datasets
+
+#open file
 file = tarfile.open('./Data/data.tar.xz') 
 # extracting file
-file.extractall('./Data')
-  
+file.extractall('./Data')  
 file.close()
 
 # open file
@@ -36,9 +39,6 @@ file = tarfile.open('./Data/data_local_concat.tar.xz')
 file.extractall('./Data')
   
 file.close()
-
-
-# In[4]:
 
 
 import pandas as pd
@@ -53,15 +53,15 @@ stats_clust=pd.read_csv('./Data/demo_clust_stats.csv')#dataset demo for clusteri
 key_mut_clust=pd.read_csv('./Data/demo_clust_key_mutations.csv')#dataset demo for clustering with the key mutation of the indian variant aligned
 
 out_path = input("Please enter the path where you want to store the result-->")
-out_name=input('Please enter the name of the folder you want to create to store the output in-->')
-
-
+out_name = input('Please enter the name of the folder you want to create to store the output in-->')
 in_path = input("Please enter the path of your fasta file, otherwise type 0 to use demo data --> ")
+
+#Checking if the format of the input FASTA is correct
 while in_path[-6:]!='.fasta' and in_path!='0':
     print("The format of the file isn't correct. Please insert a .fasta file")
     in_path = input("Please enter the path of your fasta file, otherwise type 0 to use demo data --> ")
 
-
+#Choosing the kind of analysis
 an_type = input("\nWhich kind of analysis do you desire to perform? \n 1. Supervised classification : Labels your samples according to the known variants. More accurate and less sensitive to the outliers (Advised for files with <100 samples); \n 2. Unsupervised clustering : More sensitive to the outliers but capable of highlighting new lineages of the genome (Advised for files with >100 samples). \n\n Insert your choice (1/2) ---> ")
 while an_type != '1' and an_type != '2':
     print("TPlease type 1 or 2.")
@@ -72,22 +72,23 @@ while an_type != '1' and an_type != '2':
 if in_path != '0':
     stats, genomes_aligned, mutation_list = align_and_process(in_path,string_length=1500) #for each line in path align the genomes and store the result in 
 	#genomes aligned: the aligned genomes
-	#stats: statistics of the aligned genomes 
+	#stats: statistics of the aligned genomes mutations 
 	#mutation list :characterization of each mutation found 
-    key_mut = key_mutations(mutation_list, len(stats)) #for each line calculate the most frequent mutations
+    key_mut = key_mutations(mutation_list, len(stats)) #for each line evaluate the most frequent mutations
     export_data(stats, genomes_aligned,key_mut, name = "new") #store the data in the output folder 
 
-if in_path ==  '0' and an_type == '1':
+#Demo of supervised classification
+if in_path ==  '0' and an_type == '1':   
 	#use demo data 
     stats = stats_class 
     genomes_aligned = genomes_aligned_class
     key_mut = key_mut_class
     export_data(stats, genomes_aligned,key_mut, name = "./Output/demo")
-    prediction=classifier(total_stats,stats)#Classifies each line of the stats dataset with the corresponding variants using random forest classifier
+    prediction=classifier(total_stats,stats)  #classifies each line of the stats dataset with the corresponding variants using random forest classifier
     
     
-    
-elif in_path == '0' and an_type == '2':
+#Demo of unsupervised classification    
+elif in_path == '0' and an_type == '2':   
 	stats = stats_clust
 	genomes_aligned = genomes_aligned_clust
 	key_mut = key_mut_clust
@@ -102,5 +103,5 @@ elif in_path == '0' and an_type == '2':
 			name = "New variant " + str(i+1) + "key mutations.csv"
 			key_mut_new.to_csv(name)
 
-shutil.copytree(os.path.join(path,'Bioinformatic_project/Output'),os.path.join(out_path,out_name))
-shutil.rmtree(os.path.join(path,'Bioinformatic_project'))
+shutil.copytree(os.path.join(path,'Bioinformatic_project/Output'),os.path.join(out_path,out_name))  #moves the output folder to the desired path
+shutil.rmtree(os.path.join(path,'Bioinformatic_project')) #remove the working folder
